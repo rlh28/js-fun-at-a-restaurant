780bc95423fc5d2ae91e9f683d58df2163d2b4a1
Ronald Head <ronaldlhead@gmail.com>
2021-12-02 21:18:28 -0700

    Pass test and refactor functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, deliveryOrders, takeOutOrders, takeOrders){
+  if(deliveryOrders.length < 3){
+    deliveryOrders.push(order);
+  }
+  return deliveryOrders;
+  return takeOutOrders.push(takeOrders);
+}
+
+function refundOrder(orderNum, deliveryOrders){
+  for(var i = 0; i < deliveryOrders.length; i++){
+    if(deliveryOrders[i].orderNumber === orderNum){
+      deliveryOrders.splice(i, 1)
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders){
+  var orderList = [];
+  for(i = 0; i < deliveryOrders.length; i++){
+    orderList.push(deliveryOrders[i].item);
+  }
+  return orderList.join(", ");
+}
+
+function searchOrder(deliveryOrders, orderItem){
+  var ordersArray = [];
+  for(i = 0; i < deliveryOrders.length; i++){
+    ordersArray.push(deliveryOrders[i].item)
+  }
+  if(ordersArray.includes(orderItem)){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }

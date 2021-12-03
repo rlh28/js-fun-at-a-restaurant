43b34eb26fd5485dec88c54bbff4f7e55d7017d6
Ronald Head <ronaldlhead@gmail.com>
2021-12-02 19:16:07 -0700

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+};
 
+function createMenuItem(menuItemName, price, type){
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+
+  return menuItem;
 }
 
+function addIngredients(food, ingredients){
+   if(!ingredients.includes(food)){
+     ingredients.push(food);
+   }
+  return ingredients;
+}
+
+function formatPrice(initialPrice){
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(decreasedPrice){
+  return decreasedPrice - (decreasedPrice*0.10);
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-

c6919cf0158a396745735d14707b05506d5684ea
Ronald Head <ronaldlhead@gmail.com>
2021-12-04 14:00:57 -0700

    Work on Chef.js and Chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,24 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant, custName) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(custName, timeOfDay){
+    if(timeOfDay === true){
+      return `Good morning, ${custName}!`;
+    } else {
+      return `Hello, ${custName}!`;
+    }
+  }
+  checkForFood(item){
+      if(restaurant.menus[type][i].name === item){
+        return `Sorry, we aren't serving ${item.name} today.`;
+      } else {
+        return `Yes, we're serving ${item.name} today!`;
+      }
+
   }
 }
+
 
 module.exports = Chef;
